f81e6891ad4fdfcb01d386c61f1aa06de2c7da83
suho0515 <suho0515_@naver.com>
2021-06-03 13:18:26 +0900

    created initial gui.

--- src/ur_gui_node.py
+++ src/ur_gui_node.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 import rospy
+
+form_class = uic.loadUiType("ur_gui.ui")[0]
+
+class UR_GUI(QMainWindow, form_class) :
+    def __init__(self) :
+        super(UR_GUI,self).__init__()
+        self.setupUi(self)
+
+        self.label_robotMode.setText("This is Label - Change Text")
+
 
 if __name__ == '__main__':
     rospy.init_node('ros_node', anonymous=True)
+
+    app = QApplication(sys.argv)
+    ur_gui = UR_GUI()
+    ur_gui.show()
+    app.exec_()
