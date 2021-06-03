c18eb68b7c2f357ff220c5578f21bdfec41d1ba7
suho0515 <suho0515_@naver.com>
2021-06-03 14:35:02 +0900

    display robot mode

--- src/ur_gui_node.py
+++ src/ur_gui_node.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python
 import sys
+
+# PyQt
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5.QtCore import QTimer, QTime
+
+# ROS
 import rospy
+from ur_dashboard_msgs.srv import GetRobotMode,GetRobotModeResponse
 
 form_class = uic.loadUiType("ur_gui.ui")[0]
 
 class UR_GUI(QMainWindow, form_class) :
     def __init__(self) :
+        # Initialize GUI
         super(UR_GUI,self).__init__()
         self.setupUi(self)
+        self.setWindowTitle('UR_GUI')
+
+        # Initialize Timer
+        self.timer=QTimer()
+        self.timer.timeout.connect(self.timerEvent)
+
+
+        # Initialize ROS
+        rospy.init_node('ur_gui_node', anonymous=True)
+        self.s = rospy.ServiceProxy('/ur_hardware_interface/dashboard/get_robot_mode', GetRobotMode)
+
+        # Start Timer
+        self.timer.start(1000)
+
+    def timerEvent(self):
+        resp = self.s()
+        mode = resp.robot_mode.mode
+        answer = resp.answer
+        self.label_robotMode.setText(answer)
+
 
 if __name__ == '__main__':
-    rospy.init_node('ur_gui_node', anonymous=True)
+
 
     app = QApplication(sys.argv)
     ur_gui = UR_GUI()
