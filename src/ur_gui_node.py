de42653da520da58a210b16564bbaf25da1dc6a5
suho0515 <suho0515_@naver.com>
2021-06-03 18:20:20 +0900

    Add Button control & brief thread

--- src/ur_gui_node.py
+++ src/ur_gui_node.py
@@ -5,11 +5,45 @@ import sys
 # PyQt
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-from PyQt5.QtCore import QTimer, QTime
+from PyQt5.QtCore import *
+from PyQt5 import QtCore
 
 # ROS
 import rospy
-from ur_dashboard_msgs.srv import GetRobotMode, GetProgramState, GetLoadedProgram, GetSafetyMode
+from ur_dashboard_msgs.srv import GetRobotMode, GetProgramState, GetLoadedProgram, GetSafetyMode, Load
+from std_srvs.srv import Trigger
+
+
+
+
+class TestThread(QThread):
+    # thread custom event
+    # gotta name type of data
+    threadEvent = QtCore.pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super(TestThread,self).__init__()
+        self.n = 0
+        self.main = parent
+        self.isRun = False
+
+    def run(self):
+        while self.isRun:
+            print('thread : ' + str(self.n))
+
+            # 'threadEvent' event happened
+            # can send parameter (also instance)
+            self.threadEvent.emit(self.n)
+
+            self.n += 1
+            self.sleep(1)
+
+
+
+
+
+
+
 
 # Reading UI File
 def find_data_file(filename):
@@ -42,7 +76,19 @@ class UR_GUI(QMainWindow, form_class) :
         self.s_getProgramState = rospy.ServiceProxy('/ur_hardware_interface/dashboard/program_state', GetProgramState)
         self.s_getLoadedProgram = rospy.ServiceProxy('/ur_hardware_interface/dashboard/get_loaded_program', GetLoadedProgram)
         self.s_getSafetyMode = rospy.ServiceProxy('/ur_hardware_interface/dashboard/get_safety_mode', GetSafetyMode)
+        
+        self.s_loadProgram = rospy.ServiceProxy('/ur_hardware_interface/dashboard/load_program', Load)
+        self.s_powerOn = rospy.ServiceProxy('/ur_hardware_interface/dashboard/power_on', Trigger)
+        self.s_powerOff = rospy.ServiceProxy('/ur_hardware_interface/dashboard/power_off', Trigger)
+        self.s_breakRelease = rospy.ServiceProxy('/ur_hardware_interface/dashboard/brake_release', Trigger)
+        self.s_playProgram = rospy.ServiceProxy('/ur_hardware_interface/dashboard/play', Trigger)
+        self.s_stopProgram = rospy.ServiceProxy('/ur_hardware_interface/dashboard/stop', Trigger)
 
+        self.s_loadProgram("/programs/ros.urp")
+
+        resp = self.s_getRobotMode()
+        self.mode = resp.robot_mode.mode
+        
         # Initialize Buttons
         self.pushButton_robotRunStop.setCheckable(True)
         self.pushButton_robotRunStop.toggled.connect(self.pushButton_robotRunStop_toggle)
@@ -53,9 +99,28 @@ class UR_GUI(QMainWindow, form_class) :
         # Start Timer
         self.timer.start(1000)
 
+        # Create Thread Instance
+        self.th = TestThread(self)
 
+        # Connect Thread Event
+        self.th.threadEvent.connect(self.threadEventHandler)
 
+    @pyqtSlot()
+    def threadStart(self):
+        if not self.th.isRun:
+            print('Main : Begin Thread')
+            self.th.isRun = True
+            self.th.start()
 
+    @pyqtSlot()
+    def threadStop(self):
+        if self.th.isRun:
+            print('Main : Stop Thread')
+            self.th.isRun = False
+
+    @pyqtSlot(int)
+    def threadEventHandler(self, n):
+        print('Main : threadEvent(self,' + str(n) + ')')
 
 
 
@@ -64,28 +129,81 @@ class UR_GUI(QMainWindow, form_class) :
         if(checked):
             # Running the Robot
             self.pushButton_robotRunStop.setText("Stop Robot")
+            resp = self.s_powerOn()
+            print(resp)
+            print("\n")
+            rate = rospy.Rate(1)
+
+            while True:
+                resp = self.s_getRobotMode()
+                self.mode = resp.robot_mode.mode
+                #print(self.mode)
+                rate.sleep()
+
+                if self.mode == 5:
+                    #print("stop")
+                    break
+
+            resp = self.s_breakRelease()
+            print(resp)
+            print("\n")
+
+            while True:
+                resp = self.s_getRobotMode()
+                self.mode = resp.robot_mode.mode
+                #print(self.mode)
+                rate.sleep()
+
+                if self.mode == 7:
+                    #print("stop")
+                    break
+
+            resp = self.s_playProgram()
+            print(resp)
+            print("\n")
+
+
         else:
             # Stop the Robot
             self.pushButton_robotRunStop.setText("Run Robot")
+            self.s_powerOff()
+
+            resp = self.s_stopProgram()
+            print(resp)
+            print("\n")
 
     def pushButton_processRunStop_toggle(self, checked):
         if(checked):
             # Running the Process
             self.pushButton_processRunStop.setText("Stop Process")
+
+            if not self.th.isRun:
+                print('Main : Begin Thread')
+                self.th.isRun = True
+                self.th.start()
         else:
             # Stop the Process
             self.pushButton_processRunStop.setText("Run Process")
 
+            if self.th.isRun:
+                print('Main : Stop Thread')
+                self.th.isRun = False
+
     def timerEvent(self):
         resp = self.s_getRobotMode()
-        mode = resp.robot_mode.mode
+        self.mode = resp.robot_mode.mode
         answer = resp.answer
         self.label_robotMode.setText(answer)
 
         resp = self.s_getProgramState()
-        answer = resp.state.state
-        answer = "Program state: " + answer
-        self.label_programState.setText(answer)
+        state = resp.state.state
+        state = "Program state: " + state
+        self.label_programState.setText(state)
+
+        print(state)
+        if state == "Program state: STOPPED":
+            resp = self.s_playProgram()
+            print(resp)
 
         resp = self.s_getLoadedProgram()
         answer = resp.answer
@@ -94,6 +212,15 @@ class UR_GUI(QMainWindow, form_class) :
         resp = self.s_getSafetyMode()
         answer = resp.answer
         self.label_safetyMode.setText(answer)
+
+
+
+
+
+
+
+
+
 
 
 
